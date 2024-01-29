92368e31fdd006a1ee855eb4f67bfbe668147512
Markan Katinic <mwkatinic@gmail.com>
2024-01-28 19:31:07 -0800

    Updated some dependencies, added a game rating bar

--- client/src/Components/SearchResults/index.js
+++ client/src/Components/SearchResults/index.js
@@ -11,9 +11,19 @@ const SearchPage = ({ games }) => {
             <img src={`https://images.igdb.com/igdb/image/upload/t_1080p/${game.cover.image_id}.jpg`} alt={game.name} />
           )}
           <div className="text-info">
-            <h3>{game.name}</h3>
+            <h3 className="game-name">{game.name}</h3>
             {game.rating && (
-              <p className="rating">User Rating: {game.rating.toFixed(2)}</p>
+              <div className="percent-box">
+                <div className="percent" id={`progressBar${index}`} style={{ '--rating': game.rating.toFixed(2).toString() }}>
+                  <svg>
+                    <circle cx="40" cy="40" r="40"></circle>
+                    <circle cx="40" cy="40" r="40"></circle>
+                  </svg>
+                  <div className="num">
+                    <h2 id={`rating${index}`}>{game.rating.toFixed(1).toString()}<span>%</span></h2>
+                  </div>
+                </div>
+              </div>
             )}
           </div>
         </div>
